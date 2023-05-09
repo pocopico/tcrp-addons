72339316b7430c37c1e54edd92fcfd9c2c892bd4
pocopico <pasafiogos@gmail.com>
2023-05-09 20:29:52 +0300

    Fixed links

--- disks/releases/install.sh
+++ disks/releases/install.sh
@@ -1,9 +1,3 @@
-  cp -vf  dtc /usr/sbin/
-  cp -vf  readlink /usr/sbin/
-  cp -vf  sed /usr/sbin/sed
-
-  chmod 755 /usr/sbin/dtc /usr/sbin/readlink /usr/sbin/sed
-
 
 PCI_ER="^[0-9a-fA-F]{4}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}\.[0-9a-fA-F]{1}"
 
