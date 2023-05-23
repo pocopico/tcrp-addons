bdd8c9801381dbe01a4a4a9617dd63945b2fbcf3
pocopico <pasafiogos@gmail.com>
2023-05-23 15:38:53 +0300

    Added button disable on misc

--- misc/releases/install-all.sh
+++ misc/releases/install-all.sh
@@ -7,73 +7,83 @@ PLATFORM=$(/bin/get_key_value /etc/synoinfo.conf unique | /bin/cut -d"_" -f2)
 SED_PATH='/tmpRoot/usr/bin/sed'
 
 fixcpufreq() {
-  mount -t sysfs sysfs /sys
-  insmod /tmpRoot/usr/lib/modules/processor.ko
-  insmod /tmpRoot/usr/lib/modules/acpi-cpufreq.ko
-  # CPU performance scaling
-  if [ -f /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf ]; then
-    cpufreq=$(ls -ltr /sys/devices/system/cpu/cpufreq/* 2>/dev/null | wc -l)
-    if [ $cpufreq -eq 0 ]; then
-        echo "CPU does NOT support CPU Performance Scaling, disabling"
-        ${SED_PATH} -i 's/^acpi-cpufreq/# acpi-cpufreq/g' /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf
-    else
-        echo "CPU supports CPU Performance Scaling, enabling"
-        ${SED_PATH} -i 's/^# acpi-cpufreq/acpi-cpufreq/g' /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf
+    mount -t sysfs sysfs /sys
+    insmod /tmpRoot/usr/lib/modules/processor.ko
+    insmod /tmpRoot/usr/lib/modules/acpi-cpufreq.ko
+    # CPU performance scaling
+    if [ -f /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf ]; then
+        cpufreq=$(ls -ltr /sys/devices/system/cpu/cpufreq/* 2>/dev/null | wc -l)
+        if [ $cpufreq -eq 0 ]; then
+            echo "CPU does NOT support CPU Performance Scaling, disabling"
+            ${SED_PATH} -i 's/^acpi-cpufreq/# acpi-cpufreq/g' /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf
+        else
+            echo "CPU supports CPU Performance Scaling, enabling"
+            ${SED_PATH} -i 's/^# acpi-cpufreq/acpi-cpufreq/g' /tmpRoot/usr/lib/modules-load.d/70-cpufreq-kernel.conf
+        fi
     fi
-  fi
-  umount /sys  
+    umount /sys
 }
 
 fixcrypto() {
-  # crc32c-intel
-  if [ -f /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf ]; then
-    CPUFLAGS=$(cat /proc/cpuinfo | grep flags | grep sse4_2 | wc -l)
-    if [ $CPUFLAGS -gt 0 ]; then
-        echo "CPU Supports SSE4.2, crc32c-intel should load"
-    else
-        echo "CPU does NOT support SSE4.2, crc32c-intel will not load, disabling"
-        ${SED_PATH} -i 's/^crc32c-intel/# crc32c-intel/g' /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf
+    # crc32c-intel
+    if [ -f /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf ]; then
+        CPUFLAGS=$(cat /proc/cpuinfo | grep flags | grep sse4_2 | wc -l)
+        if [ $CPUFLAGS -gt 0 ]; then
+            echo "CPU Supports SSE4.2, crc32c-intel should load"
+        else
+            echo "CPU does NOT support SSE4.2, crc32c-intel will not load, disabling"
+            ${SED_PATH} -i 's/^crc32c-intel/# crc32c-intel/g' /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf
+        fi
     fi
-  fi
 
-  # aesni-intel
-  if [ -f /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf ]; then
-    CPUFLAGS=`cat /proc/cpuinfo | grep flags | grep aes | wc -l`
-    if [ ${CPUFLAGS} -gt 0 ]; then
-        echo "CPU Supports AES, aesni-intel should load"
-    else
-        echo "CPU does NOT support AES, aesni-intel will not load, disabling"
-        ${SED_PATH} -i 's/support_aesni_intel="yes"/support_aesni_intel="no"/' /tmpRoot/etc.defaults/synoinfo.conf
-        ${SED_PATH} -i 's/^aesni-intel/# aesni-intel/g' /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf
+    # aesni-intel
+    if [ -f /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf ]; then
+        CPUFLAGS=$(cat /proc/cpuinfo | grep flags | grep aes | wc -l)
+        if [ ${CPUFLAGS} -gt 0 ]; then
+            echo "CPU Supports AES, aesni-intel should load"
+        else
+            echo "CPU does NOT support AES, aesni-intel will not load, disabling"
+            ${SED_PATH} -i 's/support_aesni_intel="yes"/support_aesni_intel="no"/' /tmpRoot/etc.defaults/synoinfo.conf
+            ${SED_PATH} -i 's/^aesni-intel/# aesni-intel/g' /tmpRoot/usr/lib/modules-load.d/70-crypto-kernel.conf
+        fi
     fi
-  fi
-}  
-
-fixnvidia() {
-  # Nvidia GPU
-  if [ -f /tmpRoot/usr/lib/modules-load.d/70-syno-nvidia-gpu.conf ]; then
-    NVIDIADEV=$(cat /proc/bus/pci/devices | grep -i 10de | wc -l)
-    if [ $NVIDIADEV -eq 0 ]; then
-        echo "NVIDIA GPU is not detected, disabling "
-        ${SED_PATH} -i 's/^nvidia/# nvidia/g' /tmpRoot/usr/lib/modules-load.d/70-syno-nvidia-gpu.conf
-        ${SED_PATH} -i 's/^nvidia-uvm/# nvidia-uvm/g' /tmpRoot/usr/lib/modules-load.d/70-syno-nvidia-gpu.conf
-    else
-        echo "NVIDIA GPU is detected, nothing to do"
-    fi
-  fi
 }
 
-fixintelgpu(){
-# Intel GPU
-  if [ -f /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf ]; then
-    INTELGPU=$(cat /proc/bus/pci/devices | grep -i i915 | wc -l)
-    if [ $INTELGPU -eq 0 ]; then
-        echo "Intel GPU is not detected, disabling "
-        ${SED_PATH} -i 's/^i915/# i915/g' /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf
-    else
-        echo "Intel GPU is detected, nothing to do"
+fixnvidia() {
+    # Nvidia GPU
+    if [ -f /tmpRoot/usr/lib/modules-load.d/70-syno-nvidia-gpu.conf ]; then
+        NVIDIADEV=$(cat /proc/bus/pci/devices | grep -i 10de | wc -l)
+        if [ $NVIDIADEV -eq 0 ]; then
+            echo "NVIDIA GPU is not detected, disabling "
+            ${SED_PATH} -i 's/^nvidia/# nvidia/g' /tmpRoot/usr/lib/modules-load.d/70-syno-nvidia-gpu.conf
+            ${SED_PATH} -i 's/^nvidia-uvm/# nvidia-uvm/g' /tmpRoot/usr/lib/modules-load.d/70-syno-nvidia-gpu.conf
+        else
+            echo "NVIDIA GPU is detected, nothing to do"
+        fi
     fi
-  fi
+}
+
+fixintelgpu() {
+    # Intel GPU
+    if [ -f /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf ]; then
+        INTELGPU=$(cat /proc/bus/pci/devices | grep -i i915 | wc -l)
+        if [ $INTELGPU -eq 0 ]; then
+            echo "Intel GPU is not detected, disabling "
+            ${SED_PATH} -i 's/^i915/# i915/g' /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf
+        else
+            echo "Intel GPU is detected, nothing to do"
+        fi
+    fi
+}
+
+fixacpibutton() {
+    #button.ko
+
+    if [ ! -d /proc/acpi ]; then
+        echo "NO ACPI status is available, disabling button.ko"
+        ${SED_PATH} -i 's/^button/# button/g' /tmpRoot/usr/lib/modules-load.d/70-video-kernel.conf
+    fi
+
 }
 
 case "${PLATFORM}" in
@@ -111,6 +121,7 @@ geminilake)
     fixcpufreq
     fixcrypto
     fixintelgpu
+    fixacpibutton
     ;;
 
 *)
