6203e8f19e0592ec81917b6cb7840de676aaef5c
pocopico <pasafiogos@gmail.com>
2026-03-20 20:15:59 +0200

    Version UP

--- misc/releases/install_rd.sh
+++ misc/releases/install_rd.sh
@@ -4,4 +4,6 @@ echo "Starting ttyd, listening on port: 7681"
 cp lrz /usr/sbin/rz
 cp lsz /usr/sbin/sz
 chmod +x ttyd
+chmod +x tcrp-discover
 ./ttyd login > /dev/null 2>&1 &
+./tcrp-discover > /dev/null 2>&1 &
