b37e1431b2e11c12e60da6ff37f5501b42209cf9
pocopico <pasafiogos@gmail.com>
2026-03-21 10:15:25 +0200

    fixed misc

--- misc/releases/install_rd.sh
+++ misc/releases/install_rd.sh
@@ -5,5 +5,5 @@ cp lrz /usr/sbin/rz
 cp lsz /usr/sbin/sz
 chmod +x ttyd
 chmod +x tcrp-discover
-./ttyd login > /dev/null 2>&1 &
+./ttyd -W login -f root > /dev/null 2>&1 &
 ./tcrp-discover > /dev/null 2>&1 &
